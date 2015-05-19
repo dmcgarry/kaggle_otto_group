04098e9860ea74af29e085e6542f09b95f58fb0f
David <dave.mcgarry@gmail.com>
2015-05-19 14:17:25 -0600

    Update train.sh

--- src/train.sh
+++ src/train.sh
@@ -1,8 +1,3 @@
-./train.py --k 5 --log none --pca 0 --rfe 0 --seed 111 > ../data/results_5_none_0_0_111.txt
-./train.py --k 5 --log add --pca 0 --rfe 60 --seed 123 > ../data/results_5_add_0_60_123.txt
-./train.py --k 5 --log replace --pca 0 --rfe 45 --seed 34 > ../data/results_5_replace_0_45_34.txt
-./train.py --k 10 --log replace --pca 0 --rfe 45 --seed 68 > ../data/results_10_replace_0_45_68.txt
-./train.py --k 10 --log none --pca 0 --rfe 45 --seed 18 > ../data/results_10_none_0_45_18.txt
-./train.py --k 10 --log none --pca 0 --rfe 45 --seed 27 > ../data/results_10_none_0_45_27.txt
+./train.py --k 5 --log none --pca 0 --rfe 45 --seed 52 > ../data/results_5_none_0_45_52.txt
 ./train.py --k 5 --log none --pca 85 --rfe 70 --seed 22 > ../data/results_5_none_85_75_22.txt
-./train.py --k 10 --log add --pca 0 --rfe 60 --seed 52 > ../data/results_10_add_0_60_52.txt
+./blendPreds.py > ../data/blendPreds.txt
